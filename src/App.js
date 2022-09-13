58c2caaedb7dff6c49650898ea2215bccf477012
K1Pro <support@k1pro.net>
2022-09-14 01:24:28 +0200

    States for collecting email and password

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log("STATE => ", email, password);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("State => ", email, password);
+  };
+
   return (
     <div className="container">
       <h1 className="display-4 text-center">Learning React</h1>
@@ -6,17 +18,27 @@ function App() {
 
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-3">
               <label className="form-label">Email address</label>
-              <input type="email" className="form-control" />
+              <input
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                type="email"
+                className="form-control"
+              />
               <div className="form-text">
                 We'll never share your email with anyone else.
               </div>
             </div>
             <div className="mb-3">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                type="password"
+                className="form-control"
+              />
             </div>
             <button type="submit" className="btn btn-primary">
               Submit
